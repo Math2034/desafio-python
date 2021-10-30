a42eb21675b24c8335f959d40494738b1588a703
Matheuzin <61693870+Math2034@users.noreply.github.com>
2021-10-30 15:16:10 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,130 @@
+print('''Escolha uma das opções:
+[ 0 ] Segunda
+[ 1 ] Terça
+[ 2 ] Quarta
+[ 3 ] Quinta
+[ 4 ] Sexta
+[ 5 ] Sábado
+[ 6 ] Domingo''')
+
+opcao_dias = str(input('Digite o dia da semana do pedido: '))
+opcao_num = int(input('Digite um prazo de 0 a 6 dias: '))
+
+#--------------------------------------------------#
+# Prazo = 0
+if ((opcao_dias == "0") and (opcao_num == 0)):
+    print('Chega hoje!')
+elif ((opcao_dias == "1") and (opcao_num == 0)):
+    print('Chega hoje!')
+elif ((opcao_dias == "2") and (opcao_num == 0)):
+    print('Chega hoje!')
+elif ((opcao_dias == "3") and (opcao_num == 0)):
+    print('Chega hoje!')
+elif ((opcao_dias == "4") and (opcao_num == 0)):
+    print('Chega hoje!')
+elif ((opcao_dias == "5") and (opcao_num == 0)):
+    print('Chega hoje!')
+elif ((opcao_dias == "6") and (opcao_num == 0)):
+    print('Chega hoje!')
+
+#--------------------------------------------------#
+# Prazo = 1
+if ((opcao_dias == "0") and (opcao_num == 1)):
+    print('Será entregue terça!')
+elif ((opcao_dias == "1") and (opcao_num == 1)):
+    print('Será entregue quarta!')
+elif ((opcao_dias == "2") and (opcao_num == 1)):
+    print('Será entregue quinta!')
+elif ((opcao_dias == "3") and (opcao_num == 1)):
+    print('Será entregue sexta!')
+elif ((opcao_dias == "4") and (opcao_num == 1)):
+    print('Será entregue sábado!')
+elif ((opcao_dias == "5") and (opcao_num == 1)):
+    print('Será entregue domingo!')
+elif ((opcao_dias == "6") and (opcao_num == 1)):
+    print('Será entregue segunda!')
+
+#--------------------------------------------------#
+# Prazo = 2
+if ((opcao_dias == "0") and (opcao_num == 2)):
+    print('Será entregue quarta!')
+elif ((opcao_dias == "1") and (opcao_num == 2)):
+    print('Será entregue quinta!')
+elif ((opcao_dias == "2") and (opcao_num == 2)):
+    print('Será entregue sexta!')
+elif ((opcao_dias == "3") and (opcao_num == 2)):
+    print('Será entregue sábado!')
+elif ((opcao_dias == "4") and (opcao_num == 2)):
+    print('Será entregue domingo!')
+elif ((opcao_dias == "5") and (opcao_num == 2)):
+    print('Será entregue segunda!')
+elif ((opcao_dias == "6") and (opcao_num == 2)):
+    print('Será entregue terça!')
+
+#--------------------------------------------------#
+# Prazo = 3
+if ((opcao_dias == "0") and (opcao_num == 3)):
+    print('Será entregue quinta!')
+elif ((opcao_dias == "1") and (opcao_num == 3)):
+    print('Será entregue sexta!')
+elif ((opcao_dias == "2") and (opcao_num == 3)):
+    print('Será entregue sábado!')
+elif ((opcao_dias == "3") and (opcao_num == 3)):
+    print('Será entregue domingo!')
+elif ((opcao_dias == "4") and (opcao_num == 3)):
+    print('Será entregue segunda!')
+elif ((opcao_dias == "5") and (opcao_num == 3)):
+    print('Será entregue terça!')
+elif ((opcao_dias == "6") and (opcao_num == 3)):
+    print('Será entregue quarta!')
+
+#--------------------------------------------------#
+# Prazo = 4
+if ((opcao_dias == "0") and (opcao_num == 4)):
+    print('Será entregue sexta!')
+elif ((opcao_dias == "1") and (opcao_num == 4)):
+    print('Será entregue sábado!')
+elif ((opcao_dias == "2") and (opcao_num == 4)):
+    print('Será entregue domingo!')
+elif ((opcao_dias == "3") and (opcao_num == 4)):
+    print('Será entregue segunda!')
+elif ((opcao_dias == "4") and (opcao_num == 4)):
+    print('Será entregue terça!')
+elif ((opcao_dias == "5") and (opcao_num == 4)):
+    print('Será entregue quarta!')
+elif ((opcao_dias == "6") and (opcao_num == 4)):
+    print('Será entregue quinta!')
+
+#--------------------------------------------------#
+# Prazo = 5
+if ((opcao_dias == "0") and (opcao_num == 5)):
+    print('Será entregue sábado!')
+elif ((opcao_dias == "1") and (opcao_num == 5)):
+    print('Será entregue domingo!')
+elif ((opcao_dias == "2") and (opcao_num == 5)):
+    print('Será entregue segunda!')
+elif ((opcao_dias == "3") and (opcao_num == 5)):
+    print('Será entregue terça!')
+elif ((opcao_dias == "4") and (opcao_num == 5)):
+    print('Será entregue quarta!')
+elif ((opcao_dias == "5") and (opcao_num == 5)):
+    print('Será entregue quinta!')
+elif ((opcao_dias == "6") and (opcao_num == 5)):
+    print('Será entregue sexta!')
+
+#--------------------------------------------------#
+# Prazo = 6
+if ((opcao_dias == "0") and (opcao_num == 6)):
+    print('Será entregue domingo!')
+elif ((opcao_dias == "1") and (opcao_num == 6)):
+    print('Será entregue segunda!')
+elif ((opcao_dias == "2") and (opcao_num == 6)):
+    print('Será entregue terça!')
+elif ((opcao_dias == "3") and (opcao_num == 6)):
+    print('Será entregue quarta!')
+elif ((opcao_dias == "4") and (opcao_num == 6)):
+    print('Será entregue quinta!')
+elif ((opcao_dias == "5") and (opcao_num == 6)):
+    print('Será entregue sexta!')
+elif ((opcao_dias == "6") and (opcao_num == 6)):
+    print('Será entregue sábado!')
